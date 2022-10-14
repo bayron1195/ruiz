a78352605d4d2f7ade37872f3030c0d987f6a0de
andres.ruiz <bayron.ruiz.mt@usa.edu.co>
2022-10-14 02:27:53 -0500

    prueba 21

--- src/main/java/com/usa/misiontic/masterclass3/controller/LibraryController.java
+++ src/main/java/com/usa/misiontic/masterclass3/controller/LibraryController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/Lib")
+@RequestMapping("/api/Audience")
 public class LibraryController {
 
     @Autowired
