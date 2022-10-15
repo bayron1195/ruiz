92d8af3a4895087b230f93744b05833404f8dc32
andres.ruiz <bayron.ruiz.mt@usa.edu.co>
2022-10-15 07:51:00 -0500

    prueba 59

--- src/main/java/com/usa/misiontic/masterclass3/entities/Score.java
+++ src/main/java/com/usa/misiontic/masterclass3/entities/Score.java
@@ -3,9 +3,11 @@ package com.usa.misiontic.masterclass3.entities;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 import javax.persistence.*;
+import java.io.Serializable;
+
 @Entity
 @Table(name = "score")
-public class Score {
+public class Score implements Serializable {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
